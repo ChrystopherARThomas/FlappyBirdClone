84a55e4aeae87122a11d69dbf34df54e7daad9f4
Chrystopher <cthomas09252003@berkeley.edu>
2025-04-20 18:51:32 -0700

    Working on movement of bird

--- Bird.py
+++ Bird.py
@@ -12,7 +12,7 @@ class Bird:
     hit = None
     score = 0
 
-    def __init__(self, color, chirp, xPos, yPos):
+    def __init__(self, color, chirp):
         if color not in Bird.colors:
             raise AttributeError
         else:
@@ -24,12 +24,21 @@ class Bird:
             self.chirp = chirp
 
         # pos used to determine whether I have hit an obstacle
-        self.xPos = xPos
-        self.yPos = yPos
+        self.xPos = 0
+        self.yPos = 0
+
+        # attributes needed to mimic gravity behavior in game
+        self.velocity = 0  # how fast bird is moving
+        self.gravity = 0.5  # how fast it bring the bird back down
+        self.flap_strength = 6  # how much of an upwards boost bird gets
 
 
-    def move(self):
-        return None
+
+
+
+    def move(self, xCor, yCor):
+        xCor += 1
+        yCor += 1
 
     def hit(self):
         return None
